2e19047f386190bdedd4bded9c99d78fc9d10236
Daniel Dale <danny.dale@gmail.com>
2021-10-26 14:27:31 -0700

    fix to enable fts when resume_from_checkpoint deprecated

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -486,7 +486,8 @@ class Trainer(
 
         self._lightning_optimizers: Optional[Dict[int, LightningOptimizer]] = None
 
-        # .validate() and .test() set this when they load a checkpoint
+        # .fit(), .validate() and .test() set this when they load a checkpoint
+        self.fit_ckpt_path: Optional[str] = None
         self.validated_ckpt_path: Optional[str] = None
         self.tested_ckpt_path: Optional[str] = None
         self.predicted_ckpt_path: Optional[str] = None
@@ -770,7 +771,10 @@ class Trainer(
 
         # TODO: ckpt_path only in v2.0
         ckpt_path = ckpt_path or self.resume_from_checkpoint
-        results = self._run(model, ckpt_path=ckpt_path)
+	self.fit_ckpt_path = self.__set_ckpt_path(
+            ckpt_path, model_provided=model, model_connected=self.lightning_module is not None
+        )
+        results = self._run(model, ckpt_path=self.fit_ckpt_path)
 
         assert self.state.stopped
         self.training = False
